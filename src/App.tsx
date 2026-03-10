b7700e4a60c2cd4f169dacd19fd8031e96b369a8
ErikDederer <erik.dederer11@gmail.com>
2026-03-10 12:26:42 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Greeting from "./components/Greeting/Greeting";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Greeting />
+      <Greeting />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
